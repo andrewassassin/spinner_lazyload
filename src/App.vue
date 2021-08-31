e569c41af6386f2e32da6006ef1572bfec060daf
andrew <r04631044@ntu.edu.tw>
2021-08-31 15:12:29 +0800

    bootstrap added

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,26 @@
 <template>
   <div id="app">
     <h1>Random User</h1>
-    <div class="person" v-for="(product, index) in productList" :key="index">
-      <div class="left">
-        <img :src="`${product.image}`" alt="">
+    <vue-simple-spinner size="medium" />
+  <section class="container">
+    <div class="row">
+      <div class="person col-md-4 my-3" v-for="(product, index) in productList" :key="index">
+        <div class="left">
+          <img :src="`${product.image}`" alt="">
+        </div>
+        <div class="right">
+          <p>{{ product.name}}}</p>
+        </div>
       </div>
-      <div class="right">
-        <p>{{ product.name}}}</p>
-        <ul>
-
-        </ul>
-      </div>
-
     </div>
+  </section>
   </div>
 </template>
 
 <script>
 import axios from 'axios'
 import moment from 'moment'
+import VueSimpleSpinner from 'vue-simple-spinner'
 export default {
   name: 'app',
   // 创建一个存放用户数据的数组
@@ -27,10 +29,13 @@ export default {
       productList: []
     }
   },
+   components: { 
+      VueSimpleSpinner
+   },
   methods: {
     // axios请求接口获取数据
     getInitialUsers() {
-      for (var i = 0; i < 5; i++) {
+      for (var i = 0; i < 6; i++) {
         axios.get(`https://x-home.pcpogo.com/homex/product.php?RDEBUG=andrewc`).then(response => {
           console.log('response.data',response.data[0])
           this.productList.push(response.data[0])
@@ -46,7 +51,7 @@ export default {
       let isLoading = false
       window.onscroll = () => {
         // 距离底部200px时加载一次
-        let bottomOfWindow = document.documentElement.offsetHeight - document.documentElement.scrollTop - window.innerHeight <= 200
+        let bottomOfWindow = document.documentElement.offsetHeight - document.documentElement.scrollTop - window.innerHeight <= 300
         if (bottomOfWindow && isLoading == false) {
           isLoading = true
           axios.get(`https://x-home.pcpogo.com/homex/product.php?RDEBUG=andrewc`).then(response => {
@@ -68,13 +73,14 @@ export default {
 </script>
 
 <style lang="scss">
+@import '~@fortawesome/fontawesome-free/css/fontawesome.css';
 .person {
   background: #ccc;
   border-radius: 2px;
-  width: 20%;
   margin: 0 auto 15px auto;
   padding: 15px;
   animation:fade 2s;
+
 
   img {
     width: 100%;
@@ -100,6 +106,17 @@ export default {
   100%{
     opacity: 1;
   }
+}
+
+
+
+*{
+  box-sizing: border-box;
+}
+
+
+body {
+    margin: 0;
 }
 
 </style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 
 
+//具有bootstrap js的功能
+import 'bootstrap/dist/js/bootstrap.min'
+//引用bootstrap css的功能
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+Vue.use(BootstrapVue)
 new Vue({
   el: '#app',
   render: h => h(App)
