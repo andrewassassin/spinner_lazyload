5d60e28c17c04bf9010691ee08d4e186a70de9e3
andrew <r04631044@ntu.edu.tw>
2021-09-13 17:21:39 +0800

    img loaded spinner

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,8 @@
     <div class="row">
       <div class="person col-md-4 my-3" v-for="(product, index) in threeList" :key="index">
         <div class="left">
-          <b-spinner :id="`${index}`" class="m-5 spin" v-bind:style="spin(index)" label="Busy" ></b-spinner>
-          <img :src="`${product.image}`" alt="">
+          <b-spinner class="m-5" v-bind:style="spin(index)" label="Busy"></b-spinner>
+          <img :src="`${product.image}`" @load="loaded" alt="">
         </div>
         <div class="right">
           <p>{{ product.name}}</p>
@@ -27,67 +27,53 @@ export default {
     return {
       productList: [],
       threeList: [],
-      // state: 'hidden',
       index:""
     }
   },
-   components: { 
-
-   },
   methods: {
      spin(idx){
-        // console.log('idx',idx)
       return {
         visibility: this.threeList[idx].visibility
         } 
     },
-    // axios请求接口获取数据
+    loaded() {
+      console.log('into loaded')
+        this.threeList.forEach(item=>{
+          item.visibility = 'hidden'
+        })
+    },
     getInitialUsers() {     
 
-        axios.get(`http://localhost/Amitproject/product.php#/`).then(response => {
-          // console.log('data',response.data)
-          // 將1~6個產品push到threeList上
-          this.productList = response.data
-          this.productList.splice(0,6).forEach(item=>{
-            item.visibility = 'hidden'
-           
-            //  item.visibility = 'hidden'
-            this.threeList.push(item)
-          })
-
-  
-        
-        })
-      
+      axios.get(`https://x-home.pcpogo.com/homex/product.php?RDEBUG=andrewc`)
+        .then(
+            response => {
+              this.productList = response.data
+              // console.log('response.data',response.data)
+              this.productList.splice(0,6).forEach(item=>{
+                item.visibility = 'hidden'
+                this.threeList.push(item)
+              })
+            }      
+         )   
     },
     scroll() {
 
       let isLoading = false
      
       window.onscroll = () => {
-         
-     
         // 距离底部200px时加载一次
         let bottomOfWindow = document.documentElement.offsetHeight - document.documentElement.scrollTop - window.innerHeight <= 200
         // let height = document.documentElement.offsetHeight - document.documentElement.scrollTop
         // console.log('bottomOfWindow',height - window.innerHeight)
         if (bottomOfWindow && isLoading == false) {
-          isLoading = true
+            isLoading = true
           
             // 理想是每滑動一次，就把res.data的接續三個產品丟進去productList，這樣throwarray的slice就可以陸續提取productList的末三項(最新三項)
             this.productList.splice(0,3).forEach(item=>{
-
-             item.visibility = 'visible'
-             
-             
-            this.threeList.push(item)
+              item.visibility = 'visible'           
+              this.threeList.push(item)
           })
-          // this.threeList[0].visibility= 'hidden'
           console.log('three',this.threeList)
-           
-  
-
-
             isLoading = false
           
         } 
@@ -98,9 +84,10 @@ export default {
   beforeMount() {
     // 在页面挂载前就发起请求
     this.getInitialUsers()
+
   },
   mounted() {
-    this.scroll(this.productList)
+    this.scroll()
   },
   computed:{
    
