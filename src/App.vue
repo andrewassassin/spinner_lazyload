b3f9beade87c72a9bea594d12d07c08dd2f59293
andrew <r04631044@ntu.edu.tw>
2021-09-17 16:34:14 +0800

    final version

--- src/App.vue
+++ src/App.vue
@@ -29,23 +29,6 @@ export default {
       threeList: [],
       index:"",
       state:'',
-      responsiveOptions: [
-			{
-				breakpoint: '1024px',
-				numVisible: 3,
-				numScroll: 3
-			},
-			{
-				breakpoint: '600px',
-				numVisible: 2,
-				numScroll: 2
-			},
-			{
-				breakpoint: '480px',
-				numVisible: 1,
-				numScroll: 1
-			}
-		]
     }
   },
   methods: {
